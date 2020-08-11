c9e001008149dbe7d20d7d8cd4a57c8dc6fb0c12
Eduardo Manuel Yupanqui Quispe <eduardo.yupanqui@outlook.com>
2020-08-11 14:01:57 -0500

    Probando

---
+++ src/OpenXmlDemo.Api/Controllers/DocumentoController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace OpenXmlDemo.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DocumentoController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<DocumentoController> _logger;
+
+        public DocumentoController(ILogger<DocumentoController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
